f3bd5f0a2ff44b70b91cb8f18ced9cd82169bf50
javier <57296818+javierlianes@users.noreply.github.com>
2025-04-19 18:51:13 +0200

    Initial commit

---
+++ wslkafka/src/main/java/com/example/wslkafka/WslkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wslkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WslkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WslkafkaApplication.class, args);
+	}
+
+}
---
+++ wslkafka/src/test/java/com/example/wslkafka/WslkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wslkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WslkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
